fe318534e366ea94cf9f3192d9d371eb4f6e7a97
Haleyou21 <107396439+Haleyou21@users.noreply.github.com>
2024-07-14 15:17:30 -0400

    Update product analysis.sql

--- product analysis.sql
+++ product analysis.sql
@@ -1,5 +1,4 @@
 -- 1)product-level sales analysis
-USE mavenfuzzyfactory;
 SELECT YEAR(created_at) AS yr, MONTH(created_at) AS mo, COUNT(DISTINCT order_id) AS number_of_sales, SUM(price_usd) AS total_revenue, SUM(price_usd-cogs_usd) AS total_margin FROM orders
 WHERE created_at<'2013-01-04'
 GROUP BY 1,2;
